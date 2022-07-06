bb82f36db6b0f2ff556f502f666422fd84215933
HUOYUE-Cynthia <guoyue@u.nus.edu>
2022-07-06 20:32:04 +0800

    clear up

--- tests/test_dictionaries.py
+++ tests/test_dictionaries.py
@@ -1,9 +1,12 @@
 import unittest
 import koopmanlib.dictionary as dictionary
+import numpy as np
+
 
 class TestDictionary(unittest.TestCase):
     """Test Suite for ``dictionary.py``
     """
+
     def test_base_dictionary(self):
         """Test trainable dictionary
         """
@@ -11,6 +14,16 @@ class TestDictionary(unittest.TestCase):
         psi_nn = dictionary.PsiNN()
         dic_config = dic_nn.get_config()
         psi_config = psi_nn.get_config()
-       
+        data_x = np.random.uniform(size=(15, 2))
+        dic_x = dic_nn(data_x)
+        psi_x = psi_nn(data_x)
+        
+        # Test shapes
+        self.assertEqual(dic_x.shape, (15, 22), 'Should be (15, 22)')
+        self.assertEqual(psi_x.shape, (15, 25), 'Should be (15, 25)')
+        
+
+
+
 if __name__ == '__main__':
     unittest.main()
--- tests/test_solvers.py
+++ tests/test_solvers.py
@@ -3,55 +3,63 @@ import koopmanlib.solver as solver
 from koopmanlib.dictionary import PsiNN, DicRBF
 import numpy as np
 
-class TestDictionary(unittest.TestCase):
+
+class TestSolver(unittest.TestCase):
     """Test Suite for ``solver.py``
     """
+
     def test_general_solver(self):
         """Test Koopman general solver
         """
         # Generate data
-        data_x_train = np.random.uniform(size=(15,2))
-        data_y_train = np.random.uniform(size=(15,2))
+        data_x_train = np.random.uniform(size=(15, 2))
+        data_y_train = np.random.uniform(size=(15, 2))
         data_train = [data_x_train, data_y_train]
 
         # Build model
 
-        # If we use rbf as a testing case, 
-        # the number of samples needs to be larger than the number of basis functions.
+        # If we use rbf as a testing case,
+        # the number of samples needs to be larger than the number of basis
+        # functions.
         rbf_basis_func = DicRBF(rbf_number=10, regularizer=1e-4)
         rbf_basis_func.build(data_x_train)
         rbf_solver = solver.KoopmanGeneralSolver(dic=rbf_basis_func,
-                                  target_dim=2,
-                                  reg=0.0)
+                                                 target_dim=2,
+                                                 reg=0.0)
         rbf_solver.build(data_train)
 
+        # Test the shape of modes
+        self.assertEqual(rbf_solver.modes.shape, (2, 13), 'Should be (2, 13)')
 
     def test_dl_solver(self):
         """Test Koopman dictionary learning solver
         """
         # Generate data
-        data_x_train = np.random.uniform(size=(15,2))
-        data_y_train = np.random.uniform(size=(15,2))
+        data_x_train = np.random.uniform(size=(15, 2))
+        data_y_train = np.random.uniform(size=(15, 2))
         data_train = [data_x_train, data_y_train]
 
-        data_x_valid = np.random.uniform(size=(5,2))
-        data_y_valid = np.random.uniform(size=(5,2))
+        data_x_valid = np.random.uniform(size=(5, 2))
+        data_y_valid = np.random.uniform(size=(5, 2))
         data_valid = [data_x_valid, data_y_valid]
 
         # Build model
-        basis_function = PsiNN(layer_sizes=[100,100,100], n_psi_train=22)
+        basis_function = PsiNN(layer_sizes=[100, 100, 100], n_psi_train=22)
         dl_solver = solver.KoopmanDLSolver(dic=basis_function,
-                         target_dim=2,
-                         reg=0.1)
+                                           target_dim=2,
+                                           reg=0.1)
 
         dl_solver.build(data_train=data_train,
-             data_valid=data_valid,
-             epochs=3,
-             batch_size=5,
-             lr=1e-4,
-             log_interval=20,
-             lr_decay_factor=0.8)
-        
+                        data_valid=data_valid,
+                        epochs=3,
+                        batch_size=5,
+                        lr=1e-4,
+                        log_interval=20,
+                        lr_decay_factor=0.8)
+
+        # Test the shape of modes
+        self.assertEqual(dl_solver.modes.shape, (2, 25), 'Should be (2, 25)')
+
 
 if __name__ == '__main__':
     unittest.main()
